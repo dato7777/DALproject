ba105bc05ae7ed2e581a0d7a94fa46ac577d3693
David Jacob Gorelashvili <dato@Davids-MacBook-Pro.local>
2022-05-17 01:04:38 +0300

    second

--- loans.py
+++ loans.py
@@ -83,36 +83,3 @@ def loanStatus(LoanID):
                         msg="returned"
                         return render_template("status.html",overDays=overDays,msg=msg)
                     
-# @books.route('/del/<id>')
-# def b_del(id):
-#     book=execute_sql_oneResult(f"select * from Books where BookID={id}")
-#     upd_sql(f"delete from Books where BookID={id}")
-#     return b_all(f"book {book[1]} was deleted!")
-    
-# @books.route('/all')
-# def b_all(msg=""):
-#     books=execute_sql("select * from Books")
-#     context={
-#         "books":books,
-#         "msg":msg
-#     }
-#     print(execute_sql("select * from Books"))
-#     return render_template('books.html', context=context)
-
-# @books.route('/single/<id>')
-# def b_single(id):
-#     book=execute_sql_oneResult(f"select * from Books where BookID={id}")
-#     return render_template("singleBook.html", book=book)
-
-# @books.route('/single/<id>/upd',methods=['GET', 'POST'])
-# def b_upd(id):
-#     if request.method == 'GET':
-#         book=execute_sql_oneResult(f"select * from Books where BookID={id}")
-#         return render_template('updateBook.html', book=book)
-#     else: #POST
-#         Published=request.form.get('Published')
-#         Type=request.form.get('Type')
-#         sql=f"UPDATE Books SET Published={Published}, Type={Type} WHERE BookID={id}"
-#         upd_sql(sql)
-#         print(sql)
-#         return b_all("Book is updated!")
